a0f7c19090af161ba3ab572d84df9caf8369f6a7
Nick Gaya <nickgaya@users.noreply.github.com>
2017-02-17 23:49:03 -0800

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
 from setuptools import find_packages, setup
 
 
+__version__ = '1.1'
+
+
 setup(
     name='python2',
-    version='1.1',
+    version=__version__,
     author="Nicholas Gaya",
     author_email="nickgaya@users.noreply.github.com",
-    description="Library for using Python 2 within Python 3",
-    url="nickgaya.wordpress.com",
+    description="A library for running Python 2 code from a Python 3 "
+                "application.",
+    url="https://github.com/nickgaya/python2",
+    download_url="https://github.com/nickgaya/python2/tarball/{}".format(
+        __version__),
     license="MIT",
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=['python2', 'legacy', 'compatibility'],
 )
